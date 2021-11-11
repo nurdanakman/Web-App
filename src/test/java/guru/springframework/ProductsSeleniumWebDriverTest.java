d5a86714b8b5e955686e41a5ffea140404f7b491
z004dtew <z004dtew@192.168.145.1>
2021-11-11 15:04:59 +0300

    Product WebDriver Test Commit

---
+++ src/test/java/guru/springframework/ProductsSeleniumWebDriverTest.java
@@ -0,0 +1,80 @@
+package guru.springframework;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ProductsSeleniumWebDriverTest {
+
+	WebDriver driver;
+
+	@BeforeSuite(description = "test ortamı hazırlandı.")
+	public void beforeSuite() {
+		WebDriverManager.chromedriver().setup();
+
+		ChromeOptions options = new ChromeOptions();
+
+		options.addArguments("--allow-insecure-localhost");
+		options.addArguments("acceptInsecureCert");
+		options.addArguments("--ignore-certificate-errors");
+
+		driver = new ChromeDriver(options);
+	}
+
+	@BeforeClass(description = "siteye giris yapilabildi mi?")
+	public void beforeTest() {
+		driver.get("http://localhost:8086/");
+		Boolean pageControl = driver.findElement(By.linkText("Home")).isDisplayed();
+
+		assertTrue(pageControl);
+		System.out.println("test00");
+	}
+
+	@Test(description = "product list görülüyor mu?")
+	public void seeTheProductList() {
+		driver.findElement(By.linkText("Products")).click();
+
+		WebElement productList = driver.findElement(By.xpath("/html/body/div/div[2]/h2"));
+
+		assertEquals(productList.getText(), "Product List");
+	}
+
+	@Test(description = "product listesindeki bir product infolarini gösteriyor mu?", dependsOnMethods = "seeTheProductList")
+	public void viewProduct() {
+		driver.navigate().back();
+		driver.findElement(By.cssSelector("a[href='/product/1']")).click();
+
+		WebElement productInfo = driver.findElement(By.xpath("/html/body/div/h2"));
+		assertEquals(productInfo.getText(), "Product Details");
+	}
+
+	@Test(description = "producta tikladiğinda dogru product bilgisini gösteriyor mu?", dependsOnMethods = "seeTheProductList")
+	public void trueProductInfo() {
+		String actualProductID = driver.findElement(By.xpath("/html/body/div/div[2]/table/tbody/tr[3]/td[1]")).getText();
+		driver.findElement(By.cssSelector("a[href='/product/2']")).click();
+		
+		String expectedProductID = driver.findElement(By.className("form-control-static")).getText();
+
+		assertEquals(actualProductID, expectedProductID);
+	}
+
+	@AfterSuite(description = "testler bitince browser pagelerini kapatır.")
+	public void close() throws InterruptedException {
+		TimeUnit.SECONDS.sleep(2);
+		driver.close();
+	}
+
+}
